b2ddee59a780ac3b0e313c1a2cd5072139273400
Kevin Lidström <iamkewwie@gmail.com>
2024-10-30 22:26:47 +0100

    fixed and added Presence

--- app/src/client.ts
+++ app/src/client.ts
@@ -1,99 +1,98 @@
 import {
-    Client,
-    GatewayIntentBits,
-    Partials,
-    Collection,
-    ColorResolvable,
-    ClientPresenceStatus
-} from "discord.js";
+	Client,
+	GatewayIntentBits,
+	Partials,
+	Collection,
+	ColorResolvable,
+	ClientPresenceStatus,
+} from 'discord.js';
 
-import { DatabaseManager } from "./managers/databaseManager";
+import { DatabaseManager } from './managers/databaseManager';
 
-import { EventManager } from "./managers/eventManager";
-import { ComponentManager } from "./managers/componentManager";
-import { PageManager } from "./pageManager";
-import { CommandManager } from "./managers/commandManager";
-import { ScheduleManager } from "./managers/scheduleManager";
-import { ModuleManager } from "./managers/moduleManager";
+import { EventManager } from './managers/eventManager';
+import { ComponentManager } from './managers/componentManager';
+import { PageManager } from './pageManager';
+import { CommandManager } from './managers/commandManager';
+import { ScheduleManager } from './managers/scheduleManager';
+import { ModuleManager } from './managers/moduleManager';
 
 export class KiwiClient extends Client {
-    public Settings: {
-        color: ColorResolvable
-    };
-    public db: DatabaseManager;
-    
-    public EventManager: EventManager;
-    public ComponentManager: ComponentManager;
-    public CommandManager: CommandManager;
-    public ScheduleManager: ScheduleManager;
-    public ModuleManager: ModuleManager;
+	public Settings: {
+		color: ColorResolvable;
+	};
+	public db: DatabaseManager;
 
-    public Pages: PageManager;
+	public EventManager: EventManager;
+	public ComponentManager: ComponentManager;
+	public CommandManager: CommandManager;
+	public ScheduleManager: ScheduleManager;
+	public ModuleManager: ModuleManager;
 
-    constructor() {
-        super({
-            intents: [
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMembers,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildModeration,
-                GatewayIntentBits.GuildVoiceStates,
-                //GatewayIntentBits.AutoModerationExecution,
-                //GatewayIntentBits.AutoModerationConfiguration,
-            ],
-            partials: [
-                Partials.GuildMember,
-                Partials.Channel,
-                Partials.Message,
-                Partials.User,
-            ],
-            presence: {
-                status: "online" as ClientPresenceStatus,
-            }
-        });
+	public Pages: PageManager;
 
-        this.Settings = {
-            color: "#7289DA"
-        }
+	constructor() {
+		super({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMembers,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.GuildModeration,
+				GatewayIntentBits.GuildVoiceStates,
+				GatewayIntentBits.GuildPresences,
+				//GatewayIntentBits.AutoModerationExecution,
+				//GatewayIntentBits.AutoModerationConfiguration,
+			],
+			partials: [
+				Partials.GuildMember,
+				Partials.Channel,
+				Partials.Message,
+				Partials.User,
+			],
+			presence: {
+				status: 'online' as ClientPresenceStatus,
+			},
+		});
 
-        // Database Manager
-        this.db = new DatabaseManager(this);
+		this.Settings = {
+			color: '#7289DA',
+		};
 
+		// Database Manager
+		this.db = new DatabaseManager(this);
 
-        // Event Manager
-        this.EventManager = new EventManager(this);
+		// Event Manager
+		this.EventManager = new EventManager(this);
 
-        // Component Manager
-        this.ComponentManager = new ComponentManager(this);
+		// Component Manager
+		this.ComponentManager = new ComponentManager(this);
 
-        // Page Manager
-        this.Pages = new PageManager(this);
+		// Page Manager
+		this.Pages = new PageManager(this);
 
-        // Command Manager
-        this.CommandManager = new CommandManager(this);
+		// Command Manager
+		this.CommandManager = new CommandManager(this);
 
-        // Schedule Manager
-        this.ScheduleManager = new ScheduleManager(this);
+		// Schedule Manager
+		this.ScheduleManager = new ScheduleManager(this);
 
-        this.ModuleManager = new ModuleManager(this);
-    }
+		this.ModuleManager = new ModuleManager(this);
+	}
 
-    public capitalize(str: string): string {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
+	public capitalize(str: string): string {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
 
-    public createCustomId(
-        options: { 
-            customId: string,
-            valueOne?: string,
-            valueTwo?: string,
-            userId?: string,
-            ownerId?: string
-        }): string {
-            var optionOne = options.valueOne || "";
-            var optionTwo = options.valueTwo || "";
-            var userId = options.userId || "";
-            var ownerId = options.ownerId || "";
-        return `+${options.customId}+?${optionOne}?&${optionTwo}&%${userId}%=${ownerId}=`;
-    }
-};
+	public createCustomId(options: {
+		customId: string;
+		valueOne?: string;
+		valueTwo?: string;
+		userId?: string;
+		ownerId?: string;
+	}): string {
+		var optionOne = options.valueOne || '';
+		var optionTwo = options.valueTwo || '';
+		var userId = options.userId || '';
+		var ownerId = options.ownerId || '';
+		return `+${options.customId}+?${optionOne}?&${optionTwo}&%${userId}%=${ownerId}=`;
+	}
+}
