376b1553e3d814c2f1d0061b98a43949c9097dd6
Kevin Lidström <iamkewwie@gmail.com>
2024-10-31 00:00:20 +0100

    Added status table

---
+++ app/src/entities/ActivityStatus.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('activity_status')
+export class ActivityStatusEntity {
+	@PrimaryColumn({ name: 'user_id' })
+	userId: number;
+
+	@Column({ name: 'user_name' })
+	userName: string;
+
+	@Column({ name: 'status' })
+	status: string;
+
+	@CreateDateColumn({ name: 'created_at' })
+	createdAt: Date;
+}
--- app/src/migrations/1727286580382-CreateActivityTables.ts
+++ app/src/migrations/1727286580382-CreateActivityTables.ts
@@ -1,199 +1,237 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateActivityTables1727286580382 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'activity_config',
+				columns: [
+					{
+						name: 'guild_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'log_channel',
+						type: 'bigint',
+						isNullable: true,
+						unsigned: true,
+						default: null,
+					},
+					{
+						name: 'daily_active_role',
+						type: 'bigint',
+						isNullable: true,
+						unsigned: true,
+						default: null,
+					},
+					{
+						name: 'weekly_active_role',
+						type: 'bigint',
+						isNullable: true,
+						unsigned: true,
+						default: null,
+					},
+				],
+			}),
+			true
+		);
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: 'activity_config',
-                columns: [
-                    {
-                        name: 'guild_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'log_channel',
-                        type: 'bigint',
-                        isNullable: true,
-                        unsigned: true,
-                        default: null
-                    },
-                    {
-                        name: 'daily_active_role',
-                        type: 'bigint',
-                        isNullable: true,
-                        unsigned: true,
-                        default: null
-                    },
-                    {
-                        name: 'weekly_active_role',
-                        type: 'bigint',
-                        isNullable: true,
-                        unsigned: true,
-                        default: null
-                    }
-                ]
-            }), true
-        );
+		await queryRunner.createTable(
+			new Table({
+				name: 'activity_voicestates',
+				columns: [
+					{
+						name: 'guild_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'channel_id',
+						type: 'bigint',
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'joined_at',
+						type: 'datetime',
+						isNullable: false,
+						default: 'CURRENT_TIMESTAMP',
+					},
+				],
+			}),
+			true
+		);
 
-        await queryRunner.createTable(
-            new Table({
-                name: 'activity_voicestates',
-                columns: [
-                    {
-                        name: 'guild_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'user_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'channel_id',
-                        type: 'bigint',
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'joined_at',
-                        type: 'datetime',
-                        isNullable: false,
-                        default: "CURRENT_TIMESTAMP"
-                    }
-                ]
-            }), true
-        );
+		await queryRunner.createTable(
+			new Table({
+				name: 'activity_voice',
+				columns: [
+					{
+						name: 'guild_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_name',
+						type: 'varchar',
+						length: '32',
+						isNullable: false,
+					},
+					{
+						name: 'total_seconds',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'daily_seconds',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'weekly_seconds',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'monthly_seconds',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'last_update',
+						type: 'datetime',
+						isNullable: false,
+						default: 'CURRENT_TIMESTAMP',
+					},
+				],
+			}),
+			true
+		);
 
-        await queryRunner.createTable(
-            new Table({
-                name: 'activity_voice',
-                columns: [
-                    {
-                        name: 'guild_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'user_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: "user_name",
-                        type: "varchar",
-                        length: "32",
-                        isNullable: false
-                    },
-                    {
-                        name: "total_seconds",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "daily_seconds",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "weekly_seconds",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "monthly_seconds",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "last_update",
-                        type: "datetime",
-                        isNullable: false,
-                        default: "CURRENT_TIMESTAMP"
-                    }
-                ]
-            }), true
-        );
+		await queryRunner.createTable(
+			new Table({
+				name: 'activity_messages',
+				columns: [
+					{
+						name: 'guild_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_name',
+						type: 'varchar',
+						length: '32',
+						isNullable: false,
+					},
+					{
+						name: 'total_messages',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'daily_messages',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'weekly_messages',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'monthly_messages',
+						type: 'int',
+						isNullable: false,
+						default: 0,
+					},
+					{
+						name: 'last_update',
+						type: 'datetime',
+						isNullable: false,
+						default: 'CURRENT_TIMESTAMP',
+					},
+				],
+			}),
+			true
+		);
 
-        await queryRunner.createTable(
-            new Table({
-                name: 'activity_messages',
-                columns: [
-                    {
-                        name: 'guild_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: 'user_id',
-                        type: 'bigint',
-                        isPrimary: true,
-                        isNullable: false,
-                        unsigned: true
-                    },
-                    {
-                        name: "user_name",
-                        type: "varchar",
-                        length: "32",
-                        isNullable: false
-                    },
-                    {
-                        name: "total_messages",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "daily_messages",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "weekly_messages",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "monthly_messages",
-                        type: "int",
-                        isNullable: false,
-                        default: 0
-                    },
-                    {
-                        name: "last_update",
-                        type: "datetime",
-                        isNullable: false,
-                        default: "CURRENT_TIMESTAMP"
-                    }
-                ]
-            }), true
-        );
-    }
+		await queryRunner.createTable(
+			new Table({
+				name: 'activity_status',
+				columns: [
+					{
+						name: 'user_id',
+						type: 'bigint',
+						isPrimary: true,
+						isNullable: false,
+						unsigned: true,
+					},
+					{
+						name: 'user_name',
+						type: 'varchar',
+						length: '32',
+						isNullable: false,
+					},
+					{
+						name: 'status',
+						type: 'varchar',
+						length: '32',
+						isNullable: false,
+					},
+					{
+						name: 'created_at',
+						type: 'datetime',
+						isNullable: false,
+						default: 'CURRENT_TIMESTAMP',
+					},
+				],
+			}),
+			true
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('activity_config');
-        await queryRunner.dropTable('activity_voice');
-        await queryRunner.dropTable('activity_voicestates');
-        await queryRunner.dropTable('activity_messages');
-    }
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('activity_config');
+		await queryRunner.dropTable('activity_voice');
+		await queryRunner.dropTable('activity_voicestates');
+		await queryRunner.dropTable('activity_messages');
+		await queryRunner.dropTable('activity_status');
+	}
 }
