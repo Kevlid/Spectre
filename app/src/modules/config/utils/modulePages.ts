1275cd5accd0ab8b7085f863070ee17e509ad4b9
Kewi <iamkewwie@gmail.com>
2024-11-22 14:34:18 +0100

    Update modulePages.ts

--- app/src/modules/config/utils/modulePages.ts
+++ app/src/modules/config/utils/modulePages.ts
@@ -1,111 +1,39 @@
-import {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ChannelSelectMenuBuilder,
-	ChannelType,
-	RoleSelectMenuBuilder,
-	StringSelectMenuBuilder,
-	User,
-	Interaction,
-} from 'discord.js';
 import { KiwiClient } from '@/client';
-import { Emojis } from '@/emojis';
+import { Guild, User } from 'discord.js';
 
-import { ConfigSelectMenu } from '../selectmenus/configType';
-import { ConfigChannelSelectMenu as ChannelSM } from '../selectmenus/configChannel';
-import { ConfigRoleSelectMenu as RoleSM } from '../selectmenus/configRole';
-
-import { generateModuleButtons } from './generateModuleButtons';
-
-interface config {
+interface Config {
 	guildId: string;
 	pageId: string;
 	pageOwner?: User;
+	guild?: Guild;
 	isEnabled?: boolean;
 }
 
-export class modulePages {
-	private interaction: Interaction;
-	private client: KiwiClient;
-
-	constructor(interaction: Interaction, client: KiwiClient) {
-		this.interaction = interaction;
-		this.client = client;
-	}
-
-	public async beforeAll() {
-		console.log(this.client, 'Before');
-	}
-
-	public async afterAll() {}
-
-	public async getPage(id: string, config: config) {}
+interface Page {
+	id: string;
+	execute: (client: KiwiClient, config: Config) => Promise<void>;
 }
 
-interface Pages {
-	[key: string]: (
-		client: KiwiClient,
-		config: {
-			guildId: string;
-			pageId: string;
-			pageOwner?: User;
-		}
-	) => Promise<void> | undefined;
+interface ModulePages {
+	setupConfig?: (client: KiwiClient, config: Config) => Promise<Config>;
+	pages: Array<Page>;
 }
 
-const pages: Pages = {
-	overview: async (client, config) => {
-		const { guildId, pageId, pageOwner } = config;
-		var guild = await client.guilds.fetch(guildId);
-		var owner = await client.users.fetch(guild.ownerId);
-		var embedDescription = [
-			`### Guild Overview`,
-			`${Emojis.ReplyTop} **Owner:** ${owner.displayName} (${owner.username})`,
-			`${Emojis.ReplyMiddle} **Members:** ${guild.memberCount}`,
-			`${Emojis.ReplyBottom} **Ping:** ${Math.round(client.ws.ping)}ms`,
-		];
+export const modulePages: ModulePages = {
+	setupConfig: async (client, config) => {
+		config.guild = await client.guilds.fetch(config.guildId);
+		config.isEnabled = await client.db.isModuleEnabled(
+			config.guildId,
+			config.pageId
+		);
+		return config;
 	},
-
-	activity: async (client, config) => {
-		const { guildId, pageId, pageOwner } = config;
-		var guild = await client.guilds.fetch(guildId);
-		var isEnabled = await client.db.repos.guildModules.findOneBy({
-			guildId: guildId,
-			moduleId: pageId,
-		});
-		var actConf = await client.db.repos.activityConfig.findOneBy({
-			guildId: guildId,
-		});
-		var embedDescription = [
-			`### Activity Module`,
-			`${Emojis.ReplyTop} **Enabled:** ${isEnabled ? 'True' : 'False'}`,
-			`${Emojis.ReplyMiddle} **Log Channel:** ${
-				actConf.logChannel ? `<#${actConf.logChannel}>` : 'Not Set'
-			}`,
-		];
-	},
-
-	verification: async (client, config) => {
-		const { guildId, pageId, pageOwner } = config;
-		var guild = await client.guilds.fetch(guildId);
-		var isEnabled = await client.db.repos.guildModules.findOneBy({
-			guildId: guildId,
-			moduleId: pageId,
-		});
-		var verConf = await client.db.repos.verificationConfig.findOneBy({
-			guildId: guildId,
-		});
-		var embedDescription = [
-			`### Verification Module`,
-			`${Emojis.ReplyTop} **Enabled:** ${isEnabled ? 'True' : 'False'}`,
-			`${Emojis.ReplyMiddle} **Log Channel:** ${
-				verConf.logChannelId ? `<#${verConf.logChannelId}>` : 'Not Set'
-			}`,
-			`${Emojis.ReplyBottom} **Verification Role:** ${
-				verConf.verificationRoleId
-					? `<@&${verConf.verificationRoleId}>`
-					: 'Not Set'
-			}`,
-		];
-	},
+	pages: [
+		{
+			id: 'overview',
+			execute: async (client, config) => {
+				const { guildId, pageId, pageOwner, guild, isEnabled } = config;
+			},
+		},
+	],
 };
