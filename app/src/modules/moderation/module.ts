30393d525eea11cfde85ed52ebe0510869ee8b51
Kewi <iamkewwie@gmail.com>
2024-12-01 12:36:30 +0100

    added delete many command

--- app/src/modules/moderation/module.ts
+++ app/src/modules/moderation/module.ts
@@ -2,9 +2,10 @@ import { Module } from '@/types/module';
 
 // Prefix Commands
 import { CleanPrefix } from './prefix/clean';
+import { DeleteManyPrefix } from './prefix/delete-many';
 import { VoiceKickPrefix } from './prefix/voice-kick';
 
 export const ModerationModule: Module = {
 	id: 'moderation',
-	prefixCommands: [CleanPrefix, VoiceKickPrefix],
+	prefixCommands: [CleanPrefix, DeleteManyPrefix, VoiceKickPrefix],
 };
