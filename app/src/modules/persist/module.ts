9757d8e8dc96d2f49c617efff6729ed6cb9b213a
Kewi <iamkewwie@gmail.com>
2024-12-03 18:52:26 +0100

    added auto delete for roles when they are in removed but still tracked by users

--- app/src/modules/persist/module.ts
+++ app/src/modules/persist/module.ts
@@ -3,8 +3,9 @@ import { Module } from '@/types/module';
 // Events
 import { GuildMemberAdd } from './events/guildMemberAdd';
 import { GuildMemberUpdate } from './events/guildMemberUpdate';
+import { GuildRoleDelete } from './events/guildRoleDelete';
 
 export const PersistModule: Module = {
 	id: 'persist',
-	events: [GuildMemberAdd, GuildMemberUpdate],
+	events: [GuildMemberAdd, GuildMemberUpdate, GuildRoleDelete],
 };
