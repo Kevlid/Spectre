908188a48a97921e7bfb49c2fe0be7661c1ffbc6
Kewi <iamkewwie@gmail.com>
2024-12-03 15:30:29 +0100

    added persist to modules

--- app/src/modules/modules.ts
+++ app/src/modules/modules.ts
@@ -5,10 +5,12 @@ import { ConfigModule } from './config/module';
 import { ActivityModule } from './activity/module';
 import { ListModule } from './list/module';
 import { ModerationModule } from './moderation/module';
+import { PersistModule } from './persist/module';
 
 export const ClientModules: Module[] = [
 	ConfigModule,
 	ActivityModule,
 	ListModule,
 	ModerationModule,
+	PersistModule,
 ];
