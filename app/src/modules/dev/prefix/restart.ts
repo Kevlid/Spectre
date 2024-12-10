7ea92f776c78ebbb9250d9c6db38fb0f8989b2c3
Kewi <iamkewwie@gmail.com>
2024-12-10 10:17:55 +0100

    Update restart.ts

--- app/src/modules/dev/prefix/restart.ts
+++ app/src/modules/dev/prefix/restart.ts
@@ -17,49 +17,6 @@ export const RestartPrefix: PrefixCommand = {
 	async execute(client, message, commandOptions) {
 		await message.reply('Restarting...');
 
-		// Save all voice data
-		var voiceStates = await client.db.repos.activityVoicestates.find();
-		for (var voiceState of voiceStates) {
-			var seconds =
-				(new Date().getTime() - voiceState.lastUpdate.getTime()) / 1000;
-			await client.db.repos.activityVoicestates.delete({
-				guildId: voiceState.guildId,
-				userId: voiceState.userId,
-			});
-
-			var userVoice = await client.db.repos.activityVoice.findOneBy({
-				guildId: voiceState.guildId,
-				userId: voiceState.userId,
-			});
-			var user = await client.users.fetch(voiceState.userId);
-
-			if (!userVoice) {
-				await client.db.repos.activityVoice.insert({
-					guildId: voiceState.guildId,
-					userId: voiceState.userId,
-					userName: user.username,
-					totalSeconds: seconds,
-					dailySeconds: seconds,
-					weeklySeconds: seconds,
-					monthlySeconds: seconds,
-				});
-			} else {
-				await client.db.repos.activityVoice.update(
-					{
-						guildId: voiceState.guildId,
-						userId: voiceState.userId,
-					},
-					{
-						userName: user.username,
-						totalSeconds: userVoice.totalSeconds + seconds,
-						dailySeconds: userVoice.dailySeconds + seconds,
-						weeklySeconds: userVoice.weeklySeconds + seconds,
-						monthlySeconds: userVoice.monthlySeconds + seconds,
-					}
-				);
-			}
-		}
-
 		if (env.RESTART_WEBHOOK_URL) {
 			fetch(env.RESTART_WEBHOOK_URL);
 		} else {
