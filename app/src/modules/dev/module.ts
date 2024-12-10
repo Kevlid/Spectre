144761e9b20239b600398fb7a5156f15be3dfca6
Kewi <iamkewwie@gmail.com>
2024-12-10 10:24:54 +0100

    added command to module

--- app/src/modules/dev/module.ts
+++ app/src/modules/dev/module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@/types/module';
 
 import { CleanDatabasePrefix } from './prefix/clean-database';
+import { RandomColorPrefix } from './prefix/random-color';
 import { RestartPrefix } from './prefix/restart';
 
 export const DevModule: Module = {
 	id: 'dev',
 	developerOnly: true,
-	prefixCommands: [CleanDatabasePrefix, RestartPrefix],
+	prefixCommands: [CleanDatabasePrefix, RandomColorPrefix, RestartPrefix],
 };
