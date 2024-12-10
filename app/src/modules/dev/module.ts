5b69a833fcbc94a6bc2492f5f3c4910f5456f8a0
Kewi <iamkewwie@gmail.com>
2024-12-10 10:09:13 +0100

    added clean database command

--- app/src/modules/dev/module.ts
+++ app/src/modules/dev/module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@/types/module';
 
+import { CleanDatabasePrefix } from './prefix/clean-database';
 import { RestartPrefix } from './prefix/restart';
 
 export const DevModule: Module = {
 	id: 'dev',
 	developerOnly: true,
-	prefixCommands: [RestartPrefix],
+	prefixCommands: [CleanDatabasePrefix, RestartPrefix],
 };
---
+++ app/src/modules/dev/prefix/clean-database.ts
@@ -0,0 +1,36 @@
+import { KiwiClient } from '@/client';
+import { ConfigOptionTypes, PrefixCommand } from '@/types/command';
+
+export const CleanDatabasePrefix: PrefixCommand = {
+	config: {
+		name: 'clean-database',
+		description: 'Clears a guild from the database',
+		aliases: ['cleandatabase', 'clean-db', 'cleandb'],
+		options: [
+			{
+				name: 'guildId',
+				type: ConfigOptionTypes.TEXT,
+			},
+		],
+	},
+	async execute(client, message, commandOptions, guildId: string) {
+		await cleanGuild(client, guildId);
+		await message.reply('Database cleaned from ' + guildId);
+	},
+};
+
+async function cleanGuild(client: KiwiClient, guildId: string) {
+	await client.db.repos.activityConfig.delete({ guildId });
+	await client.db.repos.activityMessages.delete({ guildId });
+	await client.db.repos.activityVoice.delete({ guildId });
+	await client.db.repos.activityVoicestates.delete({ guildId });
+	await client.db.repos.guildModules.delete({ guildId });
+	await client.db.repos.listConfig.delete({ guildId });
+	await client.db.repos.moderationConfig.delete({ guildId });
+	await client.db.repos.moderationConfigRole.delete({ guildId });
+	await client.db.repos.persistConfig.delete({ guildId });
+	await client.db.repos.persistConfigRequiredRole.delete({ guildId });
+	await client.db.repos.persistConfigRole.delete({ guildId });
+	await client.db.repos.persistNickname.delete({ guildId });
+	await client.db.repos.persistUserRole.delete({ guildId });
+}
