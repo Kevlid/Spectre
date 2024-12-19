04c3708d698ca6c4c425bb54d3fb93a089f56b7b
Kevin Lidström <iamkewwie@gmail.com>
2024-12-19 22:30:17 +0100

    fixed timeout command

--- app/src/modules/moderation/prefix/timeout.ts
+++ app/src/modules/moderation/prefix/timeout.ts
@@ -1,53 +1,47 @@
-import { PrefixCommand, ConfigOptionTypes } from '@/types/command';
+import { PrefixCommand, ConfigOptionTypes } from "@/types/command";
 
-import { checkPermissions } from '../utils/checkPermissions';
-import { GuildMember, TextChannel } from 'discord.js';
+import { checkPermissions } from "../utils/checkPermissions";
+import { GuildMember, TextChannel } from "discord.js";
 
 /**
  * @type {PrefixCommand}
  */
 export const TimeoutPrefix: PrefixCommand = {
 	config: {
-		name: 'timeout',
-		description: 'Timeout a user',
-		aliases: ['time', 't'],
+		name: "timeout",
+		description: "Timeout a user",
+		aliases: ["time", "t"],
 		autoDelete: true,
 		options: [
 			{
-				name: 'member',
+				name: "member",
 				type: ConfigOptionTypes.MEMBER,
 			},
 			{
-				name: 'time',
+				name: "minutes",
 				type: ConfigOptionTypes.NUMBER,
 				maxValue: 39999,
 			},
 		],
 	},
 	async checks(client, message, commandOptions) {
-		return await checkPermissions(
-			client,
-			message.guildId,
-			message.author.id
-		);
+		return await checkPermissions(client, message.guildId, message.author.id);
 	},
-	async execute(
-		client,
-		message,
-		commandOptions,
-		member: GuildMember,
-		time: number
-	) {
+	async execute(client, message, commandOptions, member: GuildMember, minutes: number) {
 		var channel = message.channel as TextChannel;
 		if (member.user.bot) {
-			channel.send('I cannot timeout this user');
+			channel.send("I cannot timeout this user");
 		} else {
-			var minutes = time * 1000 * 60;
 			try {
-				member.timeout(minutes);
+				if (minutes <= 0) {
+					member.timeout(null);
+				} else {
+					var time = minutes * 60 * 1000;
+					member.timeout(minutes);
+				}
 			} catch (err) {
 				console.log(err);
-				channel.send('I cannot timeout this user');
+				channel.send("I cannot timeout this user");
 			}
 		}
 	},
