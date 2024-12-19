33a497311e637831d4e00e2993192eafc414023e
Kevin Lidström <iamkewwie@gmail.com>
2024-12-19 22:47:45 +0100

    Added/removed commands in moderation

--- app/src/modules/moderation/module.ts
+++ app/src/modules/moderation/module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@/types/module";
 
 // Prefix Commands
-import { CleanPrefix } from "./prefix/clean";
-import { DeleteManyPrefix } from "./prefix/delete-many";
+import { DisconnectPrefix } from "./prefix/disconnect";
 import { DragPrefix } from "./prefix/drag";
 import { TimeoutPrefix } from "./prefix/timeout";
-import { VoiceKickPrefix } from "./prefix/voice-kick";
 
 export const ModerationModule: Module = {
 	id: "moderation",
-	prefixCommands: [CleanPrefix, DeleteManyPrefix, DragPrefix, TimeoutPrefix, VoiceKickPrefix],
+	prefixCommands: [DisconnectPrefix, DragPrefix, TimeoutPrefix],
 };
