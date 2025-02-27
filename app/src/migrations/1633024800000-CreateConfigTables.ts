aa72e8322dfbdd6dfef39dba9943e07c00979681
Kewi <iamkewwie@gmail.com>
2025-02-27 12:33:08 +0100

    Added new tables

---
+++ app/src/migrations/1633024800000-CreateConfigTables.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateConfigTables1633024800000 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE guild_configs (
+                guild_id BIGINT UNSIGNED NOT NULL,
+                guild_name VARCHAR(32) NOT NULL,
+                config JSON NOT NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (guild_id)
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE guild_configs`);
+    }
+}
