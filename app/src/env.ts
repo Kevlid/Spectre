6cd981dba94482d8e3dd447cd9c8046676d85109
Kewi <iamkewwie@gmail.com>
2025-06-01 13:55:29 +0200

    Update env.ts

--- app/src/env.ts
+++ app/src/env.ts
@@ -6,7 +6,7 @@ export const env = {
 	CLIENT_ID: process.env.CLIENT_ID as string,
 	CLIENT_TOKEN: process.env.CLIENT_TOKEN as string,
 
-	PREFIX: "!",
+	PREFIX: ".",
 	STAFF_USERS: (process.env.STAFF_USERS?.split(",") || []) as string[],
 
 	RESTART_WEBHOOK_URL: process.env.RESTART_WEBHOOK_URL as string,
